7b5fe71eff36378102736f8f0ec3483002ded46a
Jkig <DerekLeroux47@gmail.com>
2023-06-11 21:37:40 -0600

    Initial Commit

---
+++ Steady Progress/Steady_ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steady_ProgressApp.swift
+//  Steady Progress
+//
+//  Created by Derek Leroux on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Steady_ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
