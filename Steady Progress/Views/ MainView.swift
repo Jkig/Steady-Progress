f0a438c39191dd5389c12f35e62fdbf144cf8660
Jkig <DerekLeroux47@gmail.com>
2023-06-21 17:15:42 -0600

    Got the smoothed data to work, and pulled it to mainViewModel

--- Steady Progress/Views/ MainView.swift
+++ Steady Progress/Views/ MainView.swift
@@ -40,24 +40,10 @@ struct model: Identifiable {
 }
 
 struct MainView: View {
-    @State var keyboardIsPresented: Bool = false
+    @StateObject var viewModel = MainViewModel()
     @State private var text = ""
     
-    // test data:
-    var weights: [Float] = [188.4, 184.5, 184.6, 185.7, 180.0, 179.4, 185.0, 181.0, 186.0, 178.7, 179.6, 186.0, 179.9, 183.6, 186.4, 182.0, 180.3, 182.7, 181.2, 179.8, 181.6, 183.1, 176.4, 179.7, 181.6, 180.5, 176.2, 174.4, 175.1, 178.1, 174.6, 180.3, 182.0, 176.2, 176.9, 179.4, 174.7, 180.5, 181.7, 180.2, 176.2, 175.9, 174.3, 177.4, 174.3, 180.6, 175.0, 174.9, 176.0, 170.1, 170.1, 172.0, 175.2, 169.4, 177.7, 177.2, 174.0, 174.2, 171.6, 176.0, 176.1, 172.3, 169.8, 168.6, 167.7, 173.3, 170.7, 173.8, 174.0, 170.0, 173.6, 173.2, 174.4, 168.1, 174.8, 174.3, 166.8, 172.9, 171.3, 168.5, 168.8, 166.5, 165.2, 169.9, 167.2, 169.7, 168.1, 167.7, 169.4, 170.8, 162.5, 164.7, 163.1, 165.3, 163.1, 164.4, 162.2, 169.1, 163.3, 169.2]
-    
-    
-    
-    var data: [model] {
-        weights.enumerated().map { index, weight in
-            model(date: index + 1, weight: weight)
-        }
-    }
-    // build smooth data from data,
-    
-    
-    // set much better in time, probs just local store as variables and recalc on every new point user adds:
-    
+
     var minVal = false ? 150 : min(150, 175)
     var maxVal = 200
     var trueWeight = 162.2
@@ -69,7 +55,7 @@ struct MainView: View {
         NavigationView{
             VStack {
                 GroupBox{
-                    Chart(data) {
+                    Chart(viewModel.data) {
                         LineMark(
                             x: .value("Month", $0.date),
                             y: .value("Weight", $0.weight)
@@ -98,7 +84,7 @@ struct MainView: View {
                 
                 Spacer()
                 
-                if keyboardIsPresented {
+                if viewModel.keyboardIsPresented {
                     // Display Toolbar View
                     HStack(alignment: .center) {
                         Button {
@@ -144,7 +130,7 @@ struct MainView: View {
                 
                 HStack {
                     
-                    NavigationLink("Edit Old", destination: EditView(data:data))
+                    NavigationLink("Edit Old", destination: EditView(data:viewModel.data))
                     
                     TextField("Measurment", text: $text)
                         .keyboardType(.decimalPad)
@@ -153,7 +139,7 @@ struct MainView: View {
             }
             .padding()
             .onReceive(keyboardPublisher) { presented in
-                self.keyboardIsPresented = presented
+                viewModel.keyboardIsPresented = presented
             }
         }
     }
